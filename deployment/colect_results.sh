9e58189dee7529a5f6cb6a0ee12a68c9eb52bb9e
Bogdan Irimie <bogdan.irimie@amorphsys.com>
2017-03-13 19:51:06 +0200

    Fix bug for remote host unknown.

--- deployment/colect_results.sh
+++ deployment/colect_results.sh
@@ -4,7 +4,7 @@ echo "Colecting results"
 
 mkdir -p final
 for remoteHost in $@; do
-	rsync -a --progress remoteHost:~/viseem/final/ ./final/
+	rsync -a --progress $remoteHost:~/viseem/final/ ./final/
 done
 
 echo "All results were colected"
