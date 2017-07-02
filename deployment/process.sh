6c2e502fd8dc6fae0bc1af37dbca5ce4365de8c5
Bogdan Irimie <bogdan.irimie@amorphsys.com>
2017-07-02 23:17:18 +0300

    Update script for new raw data directory structure.

--- deployment/process.sh
+++ deployment/process.sh
@@ -2,36 +2,50 @@
 
 echo "Stage 1 - convert pdf to jpg"
 cd raw_data
-for directoryName in */ ; do
+for directoryName in */; do
         echo "Directory name is: $directoryName"
-	mkdir -p ../processed/$directoryName
-	for pdfName in $directoryName*.pdf; do
-		# echo "Pdf name is: $pdfName"
-		pdfimages -j $pdfName ../processed/$pdfName
-	done
+        mkdir -p ../processed/$directoryName
+        cd $directoryName
+        for pdfName in *.pdf; do
+                echo "Pdf name is: $pdfName"
+                mkdir -p ../../processed/$directoryName/$(sed s/\.pdf/""/g <<< $pdfName)
+                pdfimages -j $pdfName ../../processed/$directoryName/$(sed s/\.pdf/""/g <<< $pdfName)/$pdfName
+        done
 done
 
 echo "Stage 2 - clean images"
-cd ../processed
-for directoryName in */ ; do
+cd ../../processed
+for directoryName in */; do
         echo "Directory name is: $directoryName"
-	mkdir -p ../processed2/$directoryName
-	for jpgName in $directoryName*.jpg; do
-		# echo "Jpg name is: $jpgName"
-		pngName=$(echo $jpgName | sed 's/jpg/png/g')
-		# echo "png name is $pngName"
-		../textcleaner -g -e normalize -f 30 -o 12 -s 2 $jpgName ../processed2/$pngName
-	done
+        mkdir -p ../processed2/$directoryName
+        cd $directoryName
+        for subDirectory in */; do
+                echo "Subdirectory name os: $subDirectory"
+                cd $subDirectory
+                for jpgName in *.jpg; do
+                        echo "Jpg name is: $jpgName"
+                        pngName=$(echo $jpgName | sed 's/jpg/png/g')
+                        mkdir -p ../../../processed2/$directoryName/$subDirectory
+                        echo "png name is $pngName"
+                        ../../../textcleaner -g -e normalize -f 30 -o 12 -s 2 $jpgName ../../../processed2/$directoryName/$subDirectory/$pngName
+                done
+        done
 done
 
 echo "Stage 3 - ocr on images"
-cd ../processed2
-for directoryName in */ ; do
+cd ../../../processed2
+for directoryName in */; do
         echo "Directory name is: $directoryName"
-	mkdir -p ../final/$directoryName
-	for pngName in $directoryName*.png; do
-		echo "Png name is $pngName"
-		ocrName=$(echo $pngName | sed 's/\.png//g')
-		tesseract $pngName ../final/$ocrName -l ron
-	done
+        mkdir -p ../final/$directoryName
+        cd $directoryName
+        for subDirectory in */; do
+                echo "Subdirectory name os: $subDirectory"
+                cd $subDirectory
+                for pngName in *.png; do
+                        echo "Png name is $pngName"
+                        ocrName=$(echo $pngName | sed 's/\.png//g')
+                        mkdir -p ../../../final/$directoryName/$subdirectory
+                        tesseract $pngName ../final/$directoryName/$subDirectory/$ocrName -l ron
+                done
+        done
 done
